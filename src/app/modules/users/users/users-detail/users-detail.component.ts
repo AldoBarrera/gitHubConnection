4c8b5bfddf8a56acc75619c03dd771d00bb69f75
Aldo Barrera <aldo.barrera@jalasoft.com>
2019-04-27 23:40:10 -0400

    PBI 02: Adds style to the repositories list

--- src/app/modules/users/users/users-detail/users-detail.component.ts
+++ src/app/modules/users/users/users-detail/users-detail.component.ts
@@ -8,17 +8,13 @@ import { UsersService } from '../shared/users.service';
 @Component({
   selector: 'app-' + config['users'].component.nameModule + '-form',
   templateUrl: './users-detail.component.html',
-  styleUrls: ['./users-detail.component.css']
+  styleUrls: ['./users-detail.component.less']
 })
 export class UsersDetailComponent extends CommonsComponent {
-  
 
   constructor(router: Router, route: ActivatedRoute,private usersService: UsersService) 
   {
     super(router, route, usersService);
     this.moduleName = config['users'].component.nameModule ;
-    
   }
-
-
 }
